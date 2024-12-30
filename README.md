61f37c28e4b8424d74b023ba3a2eb1dd473b5eba
harikumar <harikumar3868@gmail.com>
2024-12-30 20:08:59 +0530

    Core-functionality complete

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# web-scraping
+# Web-scraping
 Web Scraping using golang and Colly framework.
+
+Steps :
+1. Fork the repository and clone it.
+2. Run  `go get github.com/gocolly/colly` in terminal.
+
--- main.go
+++ main.go
@@ -1,9 +1,72 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+
+	"github.com/gocolly/colly" //import Colly library :)
 )
 
-func main() { 
-    fmt.Println("Hello, World!")
+
+type Product struct{
+	Url string
+	Image string
+	Name string
+	Price string
+}
+
+
+func main() {
+
+	//Our logic starts..
+	var products[] Product
+	fmt.Println("Hello, World!")
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.scrapingcourse.com"),
+	)
+
+	//before every request, yeh callback hoga
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Colly Web-Scraper visiting: ", r.URL)
+	})
+
+	//agar koi error aayega toh
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Error: ", err)
+	})
+
+	//response ko track karke print karega
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Page visited: ", r.Request.URL)
+	})
+
+	// OnHTML callback on matching with li.product
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		 product:=Product{}
+		 product.Url=e.ChildAttr("a", "href")
+		 product.Image=e.ChildAttr("img","src")
+		 product.Name=e.ChildText(".product-name")
+		 product.Price=e.ChildText(".price")
+
+		 //insert our product to products
+		 products = append(products, product)
+	})
+ 
+	//once job done, print out the array
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println(r.Request.URL, " scraped!")
+		fmt.Println()
+
+		// Print each product
+	for _, product := range products {
+		fmt.Println("Product Details:") 
+		fmt.Println("  Name: ", product.Name) 
+		fmt.Println("  Price: ", product.Price)
+		fmt.Println()
+	}
+	})
+	
+	c.Visit("https://www.scrapingcourse.com/ecommerce")
+
+	
+
 }
